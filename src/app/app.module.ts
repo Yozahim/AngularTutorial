d0ffcc5a6d646cbafa6cbf557d15c77067b01194
Sobaniec <konrad.sobaniec@capgemini.com>
2020-09-14 12:19:56 +0200

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatCheckboxModule } from '@angular/material/checkbox'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatSelectModule } from '@angular/material/select'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -60,7 +63,10 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
